66217077ebe3d5c770d6b52355d8d475aa840415
Filippo Pedrazzini <filippo.pedrazzini@joandko.io>
2023-08-30 08:58:09 +0200

    added prem support

--- scripts/indexQdrant.ts
+++ scripts/indexQdrant.ts
@@ -16,9 +16,10 @@ if (!url) throw new Error(`Expected env var QDRANT_URL`);
   const pages = docs.map((doc) => doc.pageContent);
   const metadata = docs.map((doc) => doc.metadata);
 
-  await QdrantVectorStore.fromTexts(pages, metadata,  new OpenAIEmbeddings(), {
+  await QdrantVectorStore.fromTexts(pages, metadata,  new OpenAIEmbeddings({ openAIApiKey: "random-string"}, {basePath: process.env.EMBEDDINGS_MODEL_BASE_URL ?? "http://184.105.3.16:8444/v1"}), {
     url: process.env.QDRANT_URL,
-    collectionName: "nba",
+    collectionName: "nba3",
+    collectionConfig: { vectors: {size: 384, distance: "Cosine"}}
   });
 
 })();
